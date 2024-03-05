f709360952943642ce5b3e9c37e555d6c724196d
fuyang <fuyang854771076@gmail.com>
2024-03-05 15:12:48 +0800

    fix

--- TanZhengSpider/spiders/steam_csgo.py
+++ TanZhengSpider/spiders/steam_csgo.py
@@ -63,7 +63,7 @@ class SteamSpider(scrapy.Spider):
             if datas:
                 for data in datas:
                     data['sell_price']=round(float(data.get('sell_price',0)/100)*self.USD2CNY,2)
-                    data['sale_price'] = round((float(data.get('sale_price_text', '').split(' ')[0].replace("$",'').replace(",",'')) / 100) * self.USD2CNY, 2)
+                    data['sale_price'] = round((float(data.get('sale_price_text', '').split(' ')[0].replace("$",'').replace(",",'')) ) * self.USD2CNY, 2)
                     data['asset_description']['icon_url']='https://steamcommunity-a.akamaihd.net/economy/image/'+data['asset_description']['icon_url']
                     yield data
             else:
